44735ce91eeb48435feda1ba7255dec7ea4d889f
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-01-03 23:26:31 +0300

    update results

--- scripts.js
+++ scripts.js
@@ -1,22 +1,78 @@
 // Задача 1
 
-let nums = [5, 2, 4];
-let squares = new Array();
+// let nums = [5, 2, 4];
+// let squares = new Array();
 
-console.log(nums);
-console.log(squares);
+// console.log(nums);
+// console.log(squares);
 
-nums[99] = 3;
+// nums[99] = 3;
 
-console.log(nums);
+// console.log(nums);
 
-for (let i = 0, j = 0; i < nums.length; i++){
-    if (nums[i] === undefined)
-        continue;
-    else {
-        squares[j] = Math.pow(nums[i], 2);
-        j++;
+// for (let i = 0, j = 0; i < nums.length; i++){
+//     if (nums[i] === undefined)
+//         continue;
+//     else {
+//         squares[j] = Math.pow(nums[i], 2);
+//         j++;
+//     }
+// }
+
+// console.log(squares);
+
+// Задача 2
+
+let list = [
+    {
+        product: "Apple",
+        price: 25
+    }, 
+    {
+        product: "Cherry",
+        price: 32
+    },
+    {
+        product: "Strawberry",
+        price: 45
+    },
+    {
+        product: "Banana",
+        price: 35
+    },
+    {
+        product: "Pear",
+        price: 28
+    },
+    {
+        product: "Raspberry",
+        price: 32
+    },
+    {
+        product: "Orange",
+        price: 35
     }
-}
+];
 
-console.log(squares);
+let maxData = findMaxData(list);
+
+console.log(maxData);
+
+function findMaxData(listData){
+    let arr = [];
+    for (let i = 0; i < listData.length; i++){
+        arr.push(listData[i]);
+    }
+    arr.sort((a, b) => a.price - b.price);
+    let output = [];
+    output.push(arr[arr.length - 1]);
+    for (let i = arr.length - 2; i >= 0; i--){
+        if (arr[arr.length - 1] == arr[i] && i != 0){
+            output.push(arr[i]);
+        }        
+        else {
+            break;
+        }
+    }
+    return output;
+}
